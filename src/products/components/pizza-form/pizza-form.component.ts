0146309858c8a179ab52f9c1058dc1143056095f
Deenath <deenath@Deenaths-MacBook-Pro-2.local>
2018-08-25 18:15:31 +0800

    few work

--- src/products/components/pizza-form/pizza-form.component.ts
+++ src/products/components/pizza-form/pizza-form.component.ts
@@ -5,14 +5,14 @@ import {
   EventEmitter,
   OnChanges,
   SimpleChanges,
-  ChangeDetectionStrategy,
+  ChangeDetectionStrategy
 } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   FormArray,
   FormBuilder,
-  Validators,
+  Validators
 } from '@angular/forms';
 
 import { map } from 'rxjs/operators';
@@ -26,11 +26,11 @@ import { Topping } from '../../models/topping.model';
   template: `
     <div class="pizza-form">
       <form [formGroup]="form">
-      
+
         <label>
           <h4>Pizza name</h4>
-          <input 
-            type="text" 
+          <input
+            type="text"
             formControlName="name"
             placeholder="e.g. Pepperoni"
             class="pizza-form__input"
@@ -41,7 +41,7 @@ import { Topping } from '../../models/topping.model';
             <p>Pizza must have a name</p>
           </div>
         </label>
-      
+
         <ng-content></ng-content>
 
         <label>
@@ -84,22 +84,28 @@ import { Topping } from '../../models/topping.model';
 
       </form>
     </div>
-  `,
+  `
 })
 export class PizzaFormComponent implements OnChanges {
   exists = false;
 
-  @Input() pizza: Pizza;
-  @Input() toppings: Topping[];
+  @Input()
+  pizza: Pizza;
+  @Input()
+  toppings: Topping[];
 
-  @Output() selected = new EventEmitter<number[]>();
-  @Output() create = new EventEmitter<Pizza>();
-  @Output() update = new EventEmitter<Pizza>();
-  @Output() remove = new EventEmitter<Pizza>();
+  @Output()
+  selected = new EventEmitter<number[]>();
+  @Output()
+  create = new EventEmitter<Pizza>();
+  @Output()
+  update = new EventEmitter<Pizza>();
+  @Output()
+  remove = new EventEmitter<Pizza>();
 
   form = this.fb.group({
     name: ['', Validators.required],
-    toppings: [[]],
+    toppings: [[]]
   });
 
   constructor(private fb: FormBuilder) {}
@@ -120,9 +126,9 @@ export class PizzaFormComponent implements OnChanges {
     this.form
       .get('toppings')
       .valueChanges.pipe(
-        map(toppings => toppings.map((topping: Topping) => topping.id))
+        map((toppings) => toppings.map((topping: Topping) => topping.id))
       )
-      .subscribe(value => this.selected.emit(value));
+      .subscribe((value) => this.selected.emit(value));
   }
 
   createPizza(form: FormGroup) {
